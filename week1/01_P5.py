9a75bdb52c0a497b000b86cc0380cc1bb0ba93d6
thanabodee thamasaroch <thanabodeethamasaroch@thanabodees-iMac.local>
2019-01-10 00:48:52 +0700

    rename

---
+++ week1/01_P5.py
@@ -0,0 +1,5 @@
+import math
+a=float(input())
+b=float(input())
+C=float(input())
+print("area =",(1/2)*a*b*math.sin(math.radians(C)),"(sq cm)")
---
+++ week1/01_P6.py
@@ -0,0 +1,5 @@
+import math
+a=float(input())
+b=float(input())
+C=float(input())
+print("c =",math.pow((a**2+b**2-2*a*b*math.cos(math.radians(C))),1/2),"cm.")
