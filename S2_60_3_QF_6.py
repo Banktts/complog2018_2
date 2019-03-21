53b58cff22cb525d298fb8f3f5dca0a1a0a0c81a
Banktts Tth <bankthamasaroch@gmail.com>
2019-03-21 21:10:16 +0700

    pass

--- S2_60_3_QF_6.py
+++ S2_60_3_QF_6.py
@@ -1,48 +1,37 @@
+choices={}
+max_admitted={}
+sorted_applicants={}
 unassigned=[]
 admitted={}
-choices={}
-recieve={}
 for i in range(int(input().strip())):
     tmp=input().strip().split()
     choices[tmp[0]]=tmp[1:]
     unassigned.append(tmp[0])
 for i in range(int(input().strip())):
     tmp=input().strip().split()
-    recieve[tmp[0]]=[int(tmp[1])]+tmp[2:]
+    max_admitted[tmp[0]]=int(tmp[1])
+    sorted_applicants[tmp[0]]=tmp[2:]
     admitted[tmp[0]]=[]
-while len(unassigned) >0:
-    select=unassigned.pop()
-    ChoiceHasSelect=choices[select].pop(0)
-    if recieve[ChoiceHasSelect][0] > 0:
-        admitted[ChoiceHasSelect].append(select)
-        recieve[ChoiceHasSelect][0]-=1
-    else:
-        Compare=admitted[ChoiceHasSelect]
-        Case=recieve[ChoiceHasSelect][1:]
-        st=True
-        for i in Compare:
-            if Case.index(i)>Case.index(select):
-                x=admitted[ChoiceHasSelect].pop(admitted[ChoiceHasSelect].index(i))
-                admitted[ChoiceHasSelect].append(select)
-                if len(choices[x]) !=0:
-                    unassigned.append(x)
-                st=False
+while len(unassigned) !=0:
+    sid=unassigned.pop()
+    if len(choices[sid]) !=0:
+        pid=choices[sid].pop(0)
+        admitted[pid].append(sid)
+        if max_admitted[pid] < len(admitted[pid]):
+            tmp=[]
+            n=0
+            for j in sorted_applicants[pid]:
+              if n==max_admitted[pid]:
                 break
-        if st and len(choices[select])!=0:
-            unassigned.append(select)
-try:
-    admittedKey=sorted([int(i) for i in admitted.keys()])
-except:
-    admittedKey=sorted(admitted.keys())
-for i in admittedKey:
+              elif j in admitted[pid]:
+                tmp.append(j)
+                n+=1
+            S=list(set(admitted[pid])-set(tmp))[0]
+            unassigned.append(S)
+            admitted[pid]=tmp
+for i in sorted(admitted.keys()):
     print(i,end=" ")
-    try:
-        tmp=sorted([int(i) for i in admitted[str(i)]])
-    except:
-        tmp=sorted(admitted[str(i)])
-    for j in tmp:
+    for j in sorted(admitted[i]):
         print(j,end=" ")
     print()
-
-
-
+        
